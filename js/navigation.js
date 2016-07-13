b2c4e3f47d70fe0bf5a729b30009341debdf8949
TusharSachde <tushar@wohlig.com>
2016-07-13 16:47:05 +0530

    Aaj k Baad Reviews

--- js/navigation.js
+++ js/navigation.js
@@ -1,68 +1,73 @@
 var adminURL = "";
-if(isproduction)
-{
-  adminURL =  "http://www.wohlig.co.in/demo/index.php";
-}
-else {
-  adminURL = "http://localhost/demo/index.php";
+if (isproduction) {
+    adminURL = "http://www.wohlig.co.in/demo/index.php";
+} else {
+    adminURL = "http://localhost/demo/index.php";
 }
 
 var navigationservice = angular.module('navigationservice', [])
 
 .factory('NavigationService', function() {
-  var navigation = [{
-    name: "Dashboard",
-    classis: "active",
-    anchor: "dashboard",
-    icon: "dashboard",
-    subnav: [{
-      name: "Dashboard",
-      classis: "active",
-      anchor: "dashboard",
-      icon: "dashboard"
-    }]
-  },
-  {
-    name: "Company Setup",
-    classis: "active",
-    anchor: "company",
-    icon: "users",
-    subnav: [{
-      name: "Employee",
-      classis: "active",
-      anchor: "employee-list",
-      icon: "user"
-    },
-    {
-      name: "Branch",
-      classis: "active",
-      anchor: "branch-list",
-      icon: "link"
-    }]
-  },
-  {
-    name: "General",
-    classis: "active",
-    anchor: "general",
-    icon: "sticky-note",
-    subnav: []
-  }
-];
+    var navigation = [{
+        name: "Dashboard",
+        classis: "active",
+        anchor: "dashboard",
+        icon: "dashboard",
+        subnav: [{
+            name: "Dashboard",
+            classis: "active",
+            anchor: "dashboard",
+            icon: "dashboard"
+        }]
+    }, {
+        name: "Timelines",
+        classis: "active",
+        anchor: "timeline",
+        icon: "sitemap",
+        subnav: [{
+            name: "Music Broadcast Ltd",
+            classis: "active",
+            anchor: "timeline",
+            icon: "sitemap",
+        }]
+    }, {
+        name: "Company Setup",
+        classis: "active",
+        anchor: "company",
+        icon: "users",
+        subnav: [{
+            name: "Employee",
+            classis: "active",
+            anchor: "employee-list",
+            icon: "user"
+        }, {
+            name: "Branch",
+            classis: "active",
+            anchor: "branch-list",
+            icon: "link"
+        }]
+    }, {
+        name: "General",
+        classis: "active",
+        anchor: "general",
+        icon: "sticky-note",
+        subnav: []
+    }];
 
-  return {
-    getnav: function() {
-      return navigation;
-    },
-    makeactive: function(menuname) {
-      for (var i = 0; i < navigation.length; i++) {
-        if (navigation[i].name == menuname) {
-          navigation[i].classis = "active";
-        } else {
-          navigation[i].classis = "";
-        }
-      }
-      return menuname;
-    },
+    return {
+        getnav: function() {
+            return navigation;
+        },
+        makeactive: function(menuname) {
+            for (var i = 0; i < navigation.length; i++) {
+                if (navigation[i].name == menuname) {
+                    navigation[i].classis = "active";
+                } else {
+                    navigation[i].classis = "";
+                }
+            }
+            return menuname;
+        },
 
-  };
+    };
 });
